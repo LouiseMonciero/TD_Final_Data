6f03454402dbb2054f6324058a31a6bed1c94a46
LouiseMonciero <louisemonciero@gmail.com>
2025-06-14 14:15:16 +0200

    extracting RSS avis

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TD_Final_Data
+
 Analyse des Avis et Alertes ANSSI avec Enrichissement des CVE
+
+# Extraction des donnèes
+
+Les données RSS sont extraites grace au script **extracting_RSS.py**
+Elle sont sauvegardés dans le dossier **data**
+Une version plus complète et enrichie des données est disponible dans le dossier **data_pour_TD_final**
---
+++ extracting_RSS.py
@@ -0,0 +1,35 @@
+import feedparser
+import ssl
+import time
+import requests
+import os
+import json
+import random
+
+url = "https://cert.ssi.gouv.fr/avis/feed/"
+
+if hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
+rss_feed = feedparser.parse(url)
+
+i = 0
+for entry in rss_feed.entries:
+
+    url_avis = entry.link
+    response = requests.get(url_avis.rstrip('/') + '/json/')
+
+    if response.status_code == 200:
+        avis_json = response.json()
+
+        reference = avis_json.get("reference", "avis_sans_ref")
+
+        os.makedirs("./data/avis", exist_ok=True)
+
+        with open(f"./data/avis/{reference}.json", "w", encoding='utf-8') as f:
+            json.dump(avis_json, f, ensure_ascii=False, indent=4)
+    else:
+        print(f"Erreur lors de l'accès au JSON : {response.status_code} – {response.text}")
+
+    time.sleep(random.randint(90, 140)) if (i%7 == 0) else time.sleep(random.randint(3, 8))
+    i+=1
+
